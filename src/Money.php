5d5983882d4814bbcdac56dd2f93cdd63b743095
Warren Hansen <warren@billow.co.za>
2020-05-22 11:13:09 +0200

    refac: removed deprecated money_formatter

--- src/Money.php
+++ src/Money.php
@@ -1,54 +1,68 @@
 <?php
+
 namespace Billow\Utilities;
 
-/*
- * Author Warren Hansen
- * Basic Money Class
- */
+use Illuminate\Support\Facades\Config;
+use NumberFormatter;
 
 class Money
 {
-    protected $amount;
-    protected $sub_unit = 100;
-    protected $default_fraction_digits = 2;
+  protected $amount;
+  protected $sub_unit = 100;
+  protected $default_fraction_digits = 2;
+  protected $currency_format = NumberFormatter::CURRENCY;
 
-    public function __construct($amount = 0)
-    {
-        $this->amount = $amount;
-    }
+  public function __construct($amount = 0)
+  {
+    $this->amount = $amount;
+  }
 
-    /*
+  /*
      * Create new Money Object from a string value
      * @param $amount
      * return static
      */
-    public function fromString($amount = 0)
-    {
-        return new static(intval(
-            round($this->sub_unit * round((string) $amount, $this->default_fraction_digits, PHP_ROUND_HALF_UP), 0, PHP_ROUND_HALF_UP)
-        ));
-    }
+  public function fromString($amount = 0)
+  {
+    return new static(intval(
+      round($this->sub_unit * round((string) $amount, $this->default_fraction_digits, PHP_ROUND_HALF_UP), 0, PHP_ROUND_HALF_UP)
+    ));
+  }
 
-    public function amount()
-    {
-        return intval($this->amount);
-    }
+  public function amount()
+  {
+    return intval($this->amount);
+  }
 
-    public function convertedAmount($amount = null)
-    {
-        if ($amount) $this->amount = $amount;
+  public function convertedAmount($amount = null)
+  {
+    if ($amount) $this->amount = $amount;
 
-        return round($this->amount / $this->sub_unit, $this->default_fraction_digits);
-    }
+    return round($this->amount / $this->sub_unit, $this->default_fraction_digits);
+  }
 
-    public function asCurrency($amount = null, $currencySymbol = 'R')
-    {
-        if ($amount) $this->amount = $amount;
-        return money_format($currencySymbol . ' %i', $this->convertedAmount());
-    }
+  public function asCurrency($amount = null, $currencySymbol = 'ZAR')
+  {
+    if ($amount) $this->amount = $amount;
+    return $this->getFormatter()->formatCurrency($amount, $currencySymbol);
+  }
 
-    public function newMoney(int $amount)
-    {
-        return new static($amount);
-    }
+  public function newMoney(int $amount)
+  {
+    return new static($amount);
+  }
+
+  public function setCurrencyFormat($format): Money
+  {
+    $this->currency_format = $format;
+    return $this;
+  }
+
+  public function getFormatter(): NumberFormatter
+  {
+    return (new NumberFormatter(
+      Config::get('app.locale'),
+      $this->currency_format
+    ));
+  }
 }
